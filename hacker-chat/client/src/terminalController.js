e689b4eb9009a3122c4d1b4e61dd00877295a5c6
mrrioja <lulyrioja@gmail.com>
2021-03-23 23:55:56 -0300

    :alien: Inserting colors for users

--- hacker-chat/client/src/terminalController.js
+++ hacker-chat/client/src/terminalController.js
@@ -1,6 +1,23 @@
 import ComponentsBuilder from "./components.js";
 export default class TerminalController {
+  #usersCollors = new Map();
+
   contructor() {}
+
+  #pickCollor() {
+    return "#" + (((1 << 24) * Math.random()) | 0).toString(16) + "-fg";
+  }
+
+  #getUserCollor(userName) {
+    if (this.#usersCollors.has(userName)) {
+      return this.#usersCollors.get(userName);
+    }
+
+    const color = this.#pickCollor();
+    this.#usersCollors.set(userName, color);
+
+    return color;
+  }
 
   #onInputReceived(eventEmitter) {
     return function () {
@@ -8,6 +25,20 @@ export default class TerminalController {
       console.log(message);
       this.clearValue();
     };
+  }
+
+  #onMessageReceived({ screen, chat }) {
+    return (msg) => {
+      const { userName, message } = msg;
+      const collor = this.#getUserCollor(userName);
+
+      chat.addItem(`{${collor}}{bold}${userName}{/}: ${message}`);
+      screen.render();
+    };
+  }
+
+  #registerEvents(eventEmitter, components) {
+    eventEmitter.on("message:received", this.#onMessageReceived(components));
   }
 
   async initializaTable(eventEmitter) {
@@ -20,7 +51,31 @@ export default class TerminalController {
       .setChatComponent()
       .build();
 
+    this.#registerEvents(eventEmitter, components);
+
     components.input.focus();
     components.screen.render();
+
+    setInterval(() => {
+      eventEmitter.emit("message:received", {
+        message: "Salve",
+        userName: "ManoBrow",
+      });
+
+      eventEmitter.emit("message:received", {
+        message: "quebrada",
+        userName: "IceBlue",
+      });
+
+      eventEmitter.emit("message:received", {
+        message: "iae",
+        userName: "KlJay",
+      });
+
+      eventEmitter.emit("message:received", {
+        message: "opa",
+        userName: "EdiRock",
+      });
+    }, 1000);
   }
 }
