467bdb00892e38d6c69b4eb11cf5788926445db6
Luiz Rioja <lulyrioja@gmail.com>
2021-05-04 23:55:52 -0300

    :alien: Importing the controller into the server's index

--- hacker-chat/server/src/index.js
+++ hacker-chat/server/src/index.js
@@ -1,22 +1,16 @@
 import SocketServer from "./socket.js";
+import { constants } from "./constants.js";
 import Event from "events";
+import Controller from "./controller.js";
 
 const eventEmitter = new Event();
-
-async function testServer() {
-  const options = {
-    port: 9898,
-    host: "localhost",
-    headers: {
-      Connection: "Upgrade",
-      Upgrade: "websocket",
-    },
-  };
-
-  const http = await import("http");
-}
 
 const port = process.env.PORT || 9898;
 const socketServer = new SocketServer({ port });
 const server = await socketServer.initialize(eventEmitter);
 console.log("socket server is running at", server.address().port);
+const controller = new Controller({ socketServer });
+eventEmitter.on(
+  constants.event.NEW_USER_CONNECTED,
+  controller.onNewConnection.bind(controller)
+);
