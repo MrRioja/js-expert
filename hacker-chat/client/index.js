021ddc1d00373bf59d7b181d3a90ef43005c803d
Luiz Rioja <lulyrioja@gmail.com>
2021-05-08 23:04:45 -0300

    :alien: Creating the eventManager

--- hacker-chat/client/index.js
+++ hacker-chat/client/index.js
@@ -1,5 +1,6 @@
 import Events from "events";
 import CliConfig from "./src/cliConfig.js";
+import EventManager from "./src/eventManager.js";
 import SocketClient from "./src/socket.js";
 import TerminalController from "./src/terminalController.js";
 
@@ -9,6 +10,15 @@ const config = CliConfig.parseArguments(commands);
 const componentEmitter = new Events();
 const socketClient = new SocketClient(config);
 await socketClient.initialize();
+const eventManager = new EventManager({ componentEmitter, socketClient });
+const events = eventManager.getEvents();
+socketClient.attachEvents(events);
+
+const data = {
+  roomId: config.room,
+  userName: config.username,
+};
+eventManager.joinRoomAndWaitForMessage(data);
 
 const controller = new TerminalController();
 await controller.initializaTable(componentEmitter);
